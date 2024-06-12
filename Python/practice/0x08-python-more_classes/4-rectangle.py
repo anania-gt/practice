89dca8dd21888bc645619c758931308f202c28ac
trying out tokens <ananiag2015@gmail.com>
2024-06-12 03:20:12 -0400

    85

---
+++ Python/practice/0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,58 @@
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
+
+    def area(self):
+        return self._width * self._height
+
+    def perimeter(self):
+        if self._width == 0 or self._height == 0:
+            return 0
+        return 2 * (self._width + self._height)
+
+    def __str__(self):
+        if self._width == 0 or self._height == 0:
+            return ""
+        rect_str = ""
+        for _ in range(self._height):
+            rect_str += "#" * self._width + "\n"
+        return rect_str.rstrip()
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
+
+# Example usage:
+rect = Rectangle(4, 3)
+print(rect)        # Using __str__
+print(repr(rect))  # Using __repr__
+
+# Example showing the use of eval() with repr() output:
+rect_str = repr(rect)
+new_rect = eval(rect_str)
+print(new_rect)
+print(repr(new_rect))
