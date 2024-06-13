fcd9b4387a1bc9b69de9df603e1fa7c354601276
trying out tokens <ananiag2015@gmail.com>
2024-06-13 12:03:51 -0400

    p3

---
+++ Python/practice/practice03.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    """
+    Returns the sum of a and b.
+
+    >>> add(2, 3)
+    5
+    >>> add(-1, 1)
+    0
+    >>> add(0, 0)
+    0
+    """
+    return a + b
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
